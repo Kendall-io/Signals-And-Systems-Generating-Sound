6684121aeb069082c8324f11e7bdd5349f38cb22
Kendall <kmolas@nyit.edu>
2017-11-26 03:04:55 -0500

    Foundations

---
+++ README.md
@@ -0,0 +1,13 @@
+# Signals and Systems Generating Sound
+
+## Briefing:
+Since frequencies are very important in today's world, it was constantly examined and many famous
+people have assisted with the establishment of today's analyzation tools needed to improve technology
+which does not only span music and it's industry.
+
+This project looked at the frequencies of musical notes. These musical notes is not restricted only to
+what is listened to, but also to what is heard everyday. In this project, certain frequencies were 
+used to recreate any original audio.
+
+## Future goals:
+- Make it work
---
+++ soviet.py
@@ -0,0 +1,77 @@
+from scipy import signal
+from scipy.io.wavfile import write
+import numpy as np
+
+##
+# Our given sampling rate
+# @input: Some sampling rate that is within the range of the harmonics
+sampling_rate = 44100
+
+##
+# Notes within the 4 and 5th harmonic range
+# Fundamental frequencies determined from https://en.wikipedia.org/wiki/Scientific_pitch_notation
+C5_freq = 523.25
+D5_freq = 587.33
+G4_freq = 392.00 
+A4_freq = 440.00
+F4_freq = 349.23
+E4_freq = 329.63
+C4_freq = 261.63 
+
+# Time signature of music sheet
+time_signature = 1
+
+# Duration of output wav file
+time_playing = 10
+
+# Creates a numpy array with parameters: start, end, number of samples to generate
+sampling_time = np.linspace(0.0, time_playing, time_playing*sampling_rate)
+
+## Durations of each note
+quarter_note = 1
+eighth_note_with_dot = 0.75
+sixteenth_note = 0.25
+eighth_note = 0.5
+
+# Measure 1
+measure_1_note_1 = signal.chirp(sampling_time, C5_freq, quarter_note, C5_freq)
+measure_1_note_2 = signal.chirp(sampling_time, G4_freq, eighth_note_with_dot, G4_freq)
+measure_1_note_3 = signal.chirp(sampling_time, A4_freq, sixteenth_note, A4_freq)
+measure_1_note_4 = signal.chirp(sampling_time, D5_freq, quarter_note, D5_freq)
+measure_1_note_5 = signal.chirp(sampling_time, E4_freq, eighth_note, E4_freq)
+measure_1_note_6 = signal.chirp(sampling_time, E4_freq, eighth_note, E4_freq)
+
+# Measure 2
+measure_2_note_1 = signal.chirp(sampling_time, A4_freq, quarter_note, A4_freq)
+measure_2_note_2 = signal.chirp(sampling_time, G4_freq, eighth_note_with_dot, G4_freq)
+measure_2_note_3 = signal.chirp(sampling_time, F4_freq, sixteenth_note, F4_freq)
+measure_2_note_4 = signal.chirp(sampling_time, G4_freq, quarter_note, G4_freq)
+measure_2_note_5 = signal.chirp(sampling_time, C4_freq, eighth_note, C4_freq)
+measure_2_note_6 = signal.chirp(sampling_time, C4_freq, eighth_note, C4_freq)
+
+total_measures = np.concatenate(measure_1_note_1, measure_1_note_2, measure_1_note_3, measure_1_note_4,
+ measure_1_note_5, measure_1_note_6, measure_2_note_1, measure_2_note_2, measure_2_note_3, measure_2_note_4,
+  measure_2_note_5, measure_2_note_6)
+##
+# This function creates array to be processed
+# @Returns: array with notes that are in ndarray format
+'''
+def create_measures():
+	total_measures = []
+	
+	for i in range(1,3):
+		for j in range (1,7):
+			if i == 1:
+				total_measures.append(measure_1_note_ + "j")
+			
+	
+	return np.array(total_measures)
+'''
+
+#sampletimes = np.linspace(0.0, chirp_time_s, chirp_time_s*sampleRate)
+#print(sampletimes)
+#chirpdata = signal.chirp(sampletimes, chirp_start_hz, 1, chirp_end_hz)
+audio_chirp = np.interp(total_measures, [-1.0, 1.0], [-1*2**15, 2**15-1]).astype(np.int16)
+
+write("USSR.py", 44100, total_measures)
+
