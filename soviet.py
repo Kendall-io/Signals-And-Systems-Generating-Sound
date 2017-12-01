6808bd4650101e493b03cce1f186ba55da558155
Kendall <kmolas@nyit.edu>
2017-12-01 17:48:27 -0500

    Updates

--- soviet.py
+++ soviet.py
@@ -1,6 +1,8 @@
-## Authors: Kendall Molas, Daler Norkulov
+## Authors: Daler Norkulov, Kendall Molas
 from scipy import signal
 from scipy.io.wavfile import write
+from pylab import figure, plot, show, xlabel, ylabel, subplot, grid, title, yscale, savefig, clf, pcolormesh
+import matplotlib.axes
 import numpy as np
 
 ##
@@ -20,7 +22,7 @@ E4_freq = 329.63
 C4_freq = 261.63 
 
 '''
-Time signature is a 4|4 so each note have specific durations
+Time signature is a 4/4 so each note have specific durations
 quarter note: 1s
 eighth note with dot notation : 0.75s
 eight note: 0.5s
@@ -56,7 +58,7 @@ measure_2_note_5 = signal.chirp(eighth_note_sampling_time, C4_freq, eighth_note,
 measure_2_note_6 = signal.chirp(eighth_note_sampling_time, C4_freq, eighth_note, C4_freq)
 
 # Combine into one big numpy array...
-total_measures = np.concatenate((measure_1_note_1, measure_1_note_2, measure_1_note_3, measure_1_note_4, 
+total_measures = np.hstack((measure_1_note_1, measure_1_note_2, measure_1_note_3, measure_1_note_4, 
 measure_1_note_5, measure_1_note_6, measure_2_note_1, measure_2_note_2, measure_2_note_3, measure_2_note_4, 
 measure_2_note_5, measure_2_note_6))
 
@@ -64,5 +66,33 @@ measure_2_note_5, measure_2_note_6))
 # Use of int16 at the end to prevent noise and within frequency range of 20khz
 audio = np.interp(total_measures, [-1.0, 1.0], [-1*2**15, 2**15-1]).astype(np.int16)
 
-## Output audio clip from frequencies
+# Output audio clip from frequencies
 write("USSR.wav", 44100, audio)
+
+## Plotting
+output_image_size = (7.5,3.75)
+figure(1, output_image_size)
+clf()
+
+##
+# Because there is sound constantly playing, the output of this time graph will look like a blue signal
+time_sampled = np.linspace(0, 8, np.size(audio))
+
+# Time graph
+subplot(2,1,1)
+plot(time_sampled, audio)
+grid(True)
+ylabel('y(t)')
+xlabel('Time (s)')
+show()
+
+# Frequency graph
+'''
+frequency_signal = signal.resample(audio, np.size(audio)) 
+print(frequency_signal.size)
+#frequency_signal = signal.spectrogram(audio)
+#matplotlib.axes.Axes.pcolormesh(frequency_signal)
+subplot(2,1,1)
+plot(sampling_rate, frequency_signal)
+show()
+'''
